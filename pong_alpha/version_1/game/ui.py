70848029bd5ed97eb90f1c55c112995205e6bbc5
mbattifarano <eclipse0909@gmail.com>
2012-07-02 10:50:31 -0500

    removed version_1 directory

--- pong_alpha/version_1/game/ui.py
+++
@@ -1,7 +0,0 @@
-# UI.PY - controls user interface
-
-import pyglet
-
-def game_paused(batch):
-	level_label = pyglet.text.Label(text="Pong!", 
-                                x=400, y=575,anchor_x='center',batch=alt_batch)
